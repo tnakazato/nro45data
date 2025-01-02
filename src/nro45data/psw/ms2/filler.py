26f2101e4a2c42ca4f5f59860645a5098d75fdf5
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2025-01-02 13:59:42 +0900

    implement fill_procesor

--- src/nro45data/psw/ms2/filler.py
+++ src/nro45data/psw/ms2/filler.py
@@ -70,6 +70,7 @@ def fill_observation(msfile: str, hdu: 'BinTableHDU'):
             tb.addrows()
         elif nrows > 1:
             tb.removerows(np.arange(1, nrows))
+
         tb.putcell('TELESCOPE_NAME', 0, telescope_name)
         tb.putcell('TIME_RANGE', 0, time_range)
         tb.putcell('OBSERVER', 0, observer)
@@ -79,3 +80,64 @@ def fill_observation(msfile: str, hdu: 'BinTableHDU'):
         tb.putcell('PROJECT', 0, project)
         tb.putcell('RELEASE_DATE', 0, release_date)
         tb.putcell('FLAG_ROW', 0, flag_row)
+
+
+def fill_processor(msfile: str, hdu: 'BinTableHDU'):
+    # TYPE
+    processor_type = 'SPECTROMETER'
+
+    # SUB_TYPE
+    processor_sub_type_list = []
+    # AOS-High
+    arry1 = hdu.header['ARRY1'].strip()
+    if arry1.find('1') >= 0:
+        processor_sub_type_list.append('AOS-High')
+
+    # AOS-Wide 1-10 (0-9)
+    # AOS-Ultrawide 1-5 (10-15)
+    # FX 1-5? (16-20)
+    arry2 = hdu.header['ARRY2'].strip()
+    if arry2[:10].find('1') >= 0:
+        processor_sub_type_list.append('AOS-Wide')
+
+    if arry2[10:15].find('1') >= 0:
+        processor_sub_type_list.append('AOS-Ultrawide')
+
+    if arry2[15:20].find('1') >= 0:
+        processor_sub_type_list.append('FX')
+
+    # AC45
+    arry3 = hdu.header['ARRY3'].strip()
+    arry4 = hdu.header['ARRY4'].strip()
+    if (arry3 + arry4).find('1') >= 0:
+        processor_sub_type_list.append('AC45')
+
+    LOG.debug('processor_sub_type_list: %s', processor_sub_type_list)
+    LOG.debug('arr1: %s', arry1)
+    LOG.debug('arry2: %s', arry2)
+    LOG.debug('arry3: %s', arry3)
+    LOG.debug('arry4: %s', arry4)
+
+    # TYPE_ID
+    processor_type_id = 0
+
+    # MODE_ID
+    processor_mode_id = 0
+
+    # FLAG_ROW
+    flag_row = False
+
+    with open_table(msfile + '/PROCESSOR', read_only=False) as tb:
+        nrows = tb.nrows()
+        num_sub_types = len(processor_sub_type_list)
+        if nrows < num_sub_types:
+            tb.addrows(num_sub_types - nrows)
+        elif nrows > num_sub_types:
+            tb.removerows(np.arange(num_sub_types, nrows))
+
+        for irow, processor_sub_type in enumerate(processor_sub_type_list):
+            tb.putcell('TYPE', irow, processor_type)
+            tb.putcell('SUB_TYPE', irow, processor_sub_type)
+            tb.putcell('TYPE_ID', irow, processor_type_id)
+            tb.putcell('MODE_ID', irow, processor_mode_id)
+            tb.putcell('FLAG_ROW', irow, flag_row)
