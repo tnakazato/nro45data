3ef56a20f8dc1806cfbf46bb248349d650b0ccab
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2025-01-26 08:01:08 +0900

    another update on testing TIME

--- tests/ms2_filler/test_h40.py
+++ tests/ms2_filler/test_h40.py
@@ -61,15 +61,13 @@ def test_ms2_structure_h40(msfile):
         # end time: 2024/09/25 16:03:50
         start_expected = datetime.datetime(2024, 9, 25, 15, 58, 54, tzinfo=datetime.timezone.utc)
         start_time = mjd2datetime(time_range[0])
-        # assert start_time == start_expected
-        for attr in datetime_attributes:
-            assert getattr(start_time, attr) == getattr(start_expected, attr)
+        # impose msec accuracy
+        assert abs((start_time - start_expected).total_seconds()) < 1e-3
 
         end_expected = datetime.datetime(2024, 9, 25, 16, 3, 50, tzinfo=datetime.timezone.utc)
         end_time = mjd2datetime(time_range[1])
-        # assert end_time == end_expected
-        for attr in datetime_attributes:
-            assert getattr(end_time, attr) == getattr(end_expected, attr)
+        # impose msec accuracy
+        assert abs((end_time - end_expected).total_seconds()) < 1e-3
 
     with open_table(os.path.join(msfile, "SPECTRAL_WINDOW")) as tb:
         assert tb.nrows() == 4
@@ -114,16 +112,14 @@ def test_ms2_structure_h40(msfile):
         # start time: 2024/9/25 15:59:19, integration time 1sec
         start_expected = datetime.datetime(2024, 9, 25, 15, 59, 19, 500000, tzinfo=datetime.timezone.utc)
         start_time = mjd2datetime(tb.getcell("TIME", 0))
-        # assert start_time == start_expected
-        for attr in datetime_attributes:
-            assert getattr(start_time, attr) == getattr(start_expected, attr)
+        # impose msec accuracy
+        assert abs((start_time - start_expected).total_seconds()) < 1e-3
 
         # end time: 2024/9/25 16:3:50, integration time 5sec
         end_expected = datetime.datetime(2024, 9, 25, 16, 3, 47, 500000, tzinfo=datetime.timezone.utc)
         end_time = mjd2datetime(tb.getcell("TIME", nrows - 1))
-        # assert end_time == end_expected
-        for attr in datetime_attributes:
-            assert getattr(end_time, attr) == getattr(end_expected, attr)
+        # impose msec accuracy
+        assert abs((end_time - end_expected).total_seconds()) < 1e-3
 
         # data cell shape
         for irow in range(nrows):
