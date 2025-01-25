d5712e55cd23b85f90ad3f0d4e95fbf5f4a2f02f
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2025-01-25 12:01:59 +0900

    utility function to convert MJD to datetime object

--- tests/_utils.py
+++ tests/_utils.py
@@ -1,5 +1,8 @@
+import datetime
 import os
 import uuid
+
+import nro45data.psw.ms2._casa as _casa
 
 
 def _get_data_dir():
@@ -15,3 +18,35 @@ def _generate_random_name(suffix='ms'):
             return msfile
     else:
         raise RuntimeError("Failed to generate random name")
+
+
+def mjd2datetime(mjd: float) -> datetime.datetime:
+    """Convert MJD time in sec to datetime object.
+
+    Args:
+        mjd: MJD time in sec
+
+    Returns:
+        datetime object
+    """
+    if _casa._is_casa6_available:
+        qa = _casa._casa6._quanta
+        qtime = qa.quantity(mjd, "s")
+        dtdict = qa.splitdate(qtime)
+        dtobj = datetime.datetime(
+            dtdict["year"],
+            dtdict["month"],
+            dtdict["monthday"],
+            dtdict["hour"],
+            dtdict["min"],
+            dtdict["sec"],
+            dtdict["usec"]
+        )
+    elif _casa._is_casacore_available:
+        qa = _casa._casacore._quanta
+        qtime = qa.quantity(mjd, "s")
+        dtobj = datetime.datetime.fromtimestamp(qtime.to_unix_time())
+    else:
+        raise ModuleNotFoundError("Neither casatools or python-casacore is available")
+
+    return dtobj
