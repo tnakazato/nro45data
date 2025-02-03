da6ef2b7592a168ae6229ec8559b958009183cc7
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2025-02-03 21:31:04 +0900

    refactor fill_polarization

--- src/nro45data/psw/ms2/filler/polarization.py
+++ src/nro45data/psw/ms2/filler/polarization.py
@@ -1,10 +1,11 @@
+from __future__ import annotations
+
 import logging
-from typing import TYPE_CHECKING
+from typing import Generator, TYPE_CHECKING
 
 import numpy as np
 
-from .._casa import open_table
-from .utils import fix_nrow_to, get_array_configuration, get_data_description_map
+from .utils import fill_ms_table, get_array_configuration, get_data_description_map
 
 if TYPE_CHECKING:
     from astropy.io.fits.hdu.BinTableHDU import BinTableHDU
@@ -12,7 +13,22 @@ if TYPE_CHECKING:
 LOG = logging.getLogger(__name__)
 
 
-def pol_str_to_enum(pols: list[str]) -> int:
+def pol_str_to_enum(pols: list[str]) -> list[int]:
+    """Map polarization string to polarization enum.
+
+    See following link for Stokes enumeration:
+
+    https://casacore.github.io/casacore/classcasacore_1_1Stokes.html
+
+    Args:
+        pols: List of polarization strings.
+
+    Returns:
+        List of polarization enums.
+
+    Raises:
+        AssertionError: If polarization string is not any of 'H', 'V', 'R', 'L'.
+    """
     if len(pols) == 1:
         pol_type = pols[0][-1]
         if pol_type == "H":
@@ -48,43 +64,65 @@ def pol_str_to_enum(pols: list[str]) -> int:
         AssertionError("number of polarization must be either 1 or 2")
 
 
-def _get_polarization_columns(hdu: "BinTableHDU") -> dict:
+def _get_polarization_row(hdu: BinTableHDU) -> Generator[dict, None, None]:
+    """Provide polarization row information.
+
+    Args:
+        hdu: NRO45m psw data in the form of BinTableHDU object.
+
+    Yields:
+        Dictionary containing polarization row information.
+
+    Raises:
+        AssertionError: If number of polarization is not either 1 or 2.
+    """
     array_conf = get_array_configuration(hdu)
     _, _, _, pol_map = get_data_description_map(array_conf)
-    num_pol = len(pol_map)
+    # num_pol = len(pol_map)
 
     # CORR_TYPE
-    corr_type = [np.array(pol_str_to_enum(pol_map[ipol])) for ipol in range(num_pol)]
+    corr_type = np.array(pol_str_to_enum(pol_map[0]))
 
     # CORR_PRODUCT
-    corr_product = []
-    for ipol in range(num_pol):
-        v = pol_map[ipol]
-        if len(v) == 1:
-            corr_product.append(np.array([[0], [0]]))
-        elif len(v) == 2:
-            corr_product.append(np.array([[0, 1], [0, 1]]))
-        else:
-            AssertionError("number of polarization must be either 1 or 2")
+    corr_product = None
+    v = pol_map[0]
+    if len(v) == 1:
+        corr_product = np.array([[0], [0]])
+    elif len(v) == 2:
+        corr_product = np.array([[1, 0], [0, 1]])
+    else:
+        AssertionError("number of polarization must be either 1 or 2")
 
     # NUM_CORR
-    num_corr = np.array([len(pol_map[ipol]) for ipol in range(num_pol)])
+    num_corr = len(pol_map[0])
 
     # FLAG_ROW
-    flag_row = np.zeros(num_pol, dtype=bool)
+    flag_row = False
 
-    columns = {"CORR_TYPE": corr_type, "CORR_PRODUCT": corr_product, "NUM_CORR": num_corr, "FLAG_ROW": flag_row}
+    row = {
+        "CORR_TYPE": corr_type,
+        "CORR_PRODUCT": corr_product,
+        "NUM_CORR": num_corr,
+        "FLAG_ROW": flag_row
+    }
 
-    return columns
+    yield row
 
 
-def _fill_polarization_columns(msfile: str, columns: dict):
-    with open_table(msfile + "/POLARIZATION", read_only=False) as tb:
-        num_pol = len(columns["NUM_CORR"])
-        fix_nrow_to(num_pol, tb)
+def fill_polarization(msfile: str, hdu: BinTableHDU):
+    """Fill MS POLARIZATION table.
 
-        tb.putcol("NUM_CORR", columns["NUM_CORR"])
-        tb.putcol("FLAG_ROW", columns["FLAG_ROW"])
-        for i in range(num_pol):
-            tb.putcell("CORR_TYPE", i, columns["CORR_TYPE"][i])
-            tb.putcell("CORR_PRODUCT", i, columns["CORR_PRODUCT"][i])
+    Args:
+        msfile: Name of MS file.
+        hdu: NRO45m psw data in the form of BinTableHDU object.
+    """
+    fill_ms_table(msfile, hdu, "POLARIZATION", _get_polarization_row)
+    # with open_table(msfile + "/POLARIZATION", read_only=False) as tb:
+    #     num_pol = len(columns["NUM_CORR"])
+    #     fix_nrow_to(num_pol, tb)
+
+    #     tb.putcol("NUM_CORR", columns["NUM_CORR"])
+    #     tb.putcol("FLAG_ROW", columns["FLAG_ROW"])
+    #     for i in range(num_pol):
+    #         tb.putcell("CORR_TYPE", i, columns["CORR_TYPE"][i])
+    #         tb.putcell("CORR_PRODUCT", i, columns["CORR_PRODUCT"][i])
