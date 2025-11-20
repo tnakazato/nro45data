75aab4054e9d281306580489a440ecf75aba7a13
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2025-11-20 16:57:07 +0900

    Add blank lines to be compliant with coding style

--- src/nro45data/__init__.py
+++ src/nro45data/__init__.py
@@ -9,10 +9,12 @@ __all__ = ["nqm2fits", "nqm2ms2"]
 def nqm2fits(nqmfile: str, fitsfile: str, overwrite: bool = False) -> bool:
     return psw.nqm2fits(nqmfile, fitsfile, overwrite)
 
+
 nqm2fits.__doc__ = psw.nqm2fits.__doc__
 
 
 def nqm2ms2(nqmfile: str, msfile: str, overwrite: bool = False) -> bool:
     return psw.nqm2ms2(nqmfile, msfile, overwrite)
+
 
 nqm2ms2.__doc__ = psw.nqm2ms2.__doc__
